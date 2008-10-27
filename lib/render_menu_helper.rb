a8552fb0a573f9b5629e3298c4bc266480e5dd53
Bene Tamás <bene@bitandpixel.hu>
2008-10-27 17:54:41 +0100

    Add model instruction for render_menu method

--- lib/render_menu_helper.rb
+++ lib/render_menu_helper.rb
@@ -1,13 +1,15 @@
 module ActionView
   class Base
 
-    def render_menu(dynamic_menu)
+    def render_menu(dynamic_menu, *args)
       raise 'No DynamicMenu or Item Object' unless dynamic_menu.instance_of?(DynamicMenu) or dynamic_menu.instance_of?(Item)
+      options = args.last.is_a?(Hash) ? args.last : {}
+      model = options[:model] || [:ul, :li]
       
-      content_tag :ul do
+      content_tag model.first do
         dynamic_menu.items.map do |menu_item|
-          content_tag :li, (link_to menu_item[:name], menu_item[:target]) + (menu_item.items.empty? ? '' : render_menu(menu_item))
-        end.flatten.compact.join
+          content_tag model.last, (link_to menu_item[:name], menu_item[:target]) + (menu_item.items.empty? ? '' : render_menu(menu_item, *args))
+        end
       end
     end
 
