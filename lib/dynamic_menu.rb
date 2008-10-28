c5a295c3d2db328394f83c0b474670c1805c317f
Bene Tamás <bene@bitandpixel.hu>
2008-10-28 16:57:30 +0100

    Add variable feature

--- lib/dynamic_menu.rb
+++ lib/dynamic_menu.rb
@@ -6,10 +6,14 @@ class DynamicMenu < MenuItem
 
   def initialize(*args, &block)
     options     = args.last.is_a?(Hash) ? args.pop : {}
-    @controller = options[:controller]
-    @action     = options[:action]
+    
     @items      = []
     @parent     = nil
+    
+    options.each do |key, value|
+      self.instance_variable_set("@#{key.to_s}".to_sym, value)
+    end
+
     block.call(self) if block_given?
   end
 
@@ -22,23 +26,27 @@ class Item < MenuItem
 
     options = args.last.is_a?(Hash) ? args.pop : {}
 
-    @items              = []
-    @name               = options[:name] || args[0]
-    @target             = options[:target] || args[1]
+    _parent.instance_variables.each do |i_v|
+      self.instance_variable_set(i_v.to_sym, _parent.instance_variable_get(i_v.to_sym))
+    end
+
     @parent             = _parent
-    @controller         = options[:controller] || @parent.controller
-    @action             = options[:action] || @parent.action
+
+    @items              = []
+    
+    @name               = options.delete(:name) || args[0]
+    @target             = options.delete(:target) || args[1]
+
+    options.each do |key, value|
+      self.instance_variable_set("@#{key.to_s}".to_sym, value)
+    end
+
     @enabled            = if enabled = options.delete(:enabled)
                             enabled if enabled.is_a?(TrueClass) or enabled.is_a?(FalseClass)
                             get_enabled(enabled) if enabled.is_a?(Hash) or enabled.is_a?(Array)
                           else
                             true
                           end
-    @html_options       = options[:html_options] || {}
-
-    options.each do |key, value|
-      self.instance_variable_set("@#{key.to_s}".to_sym, value)
-    end
 
     block.call(self) if block_given?
 
--- lib/menu_item.rb
+++ lib/menu_item.rb
@@ -2,8 +2,6 @@ class MenuItem
 
   attr_reader :name
   attr_reader :items
-  attr_reader :controller
-  attr_reader :action
   attr_reader :target
 
   def [](attribute)
@@ -16,7 +14,7 @@ class MenuItem
 
   def add(*args, &block)
     item = Item.new(self, *args, &block)
-    @items << item
+    @items.push item
   end
 
   def remove(*args)
@@ -43,11 +41,33 @@ class MenuItem
   protected
 
   def get_enabled(enabled)
-    if enabled.is_a?(Hash) and enabled.has_key?(:controller) and enabled.has_key?(:action)
-      controller == enabled[:controller].to_s and (action == enabled[:action].to_s or enabled[:action].to_s == 'all')
-    else
-      false
+    result_ary  = []
+    temp_ary    = []
+    if enabled.is_a?(Hash)
+      result_ary << enabled_helper(enabled)
+    elsif enabled.is_a?(Array)
+      enabled.each do |enabled_item|
+        if enabled_item.is_a?(Hash)
+          result_ary << enabled_helper(enabled_item)
+        end
+      end
     end
+    result_ary.include?(true)
+  end
+
+  private
+
+  def enabled_helper(enabled)
+    raise 'No hash argument' unless enabled.is_a?(Hash)
+    temp_ary = []
+    enabled.each do |key, value|
+      temp_ary << if value.is_a?(Array)
+                    value.map {|v| self.instance_variable_get("@#{key.to_s}".to_sym).to_s == v.to_s}.include?(true)
+      else
+        self.instance_variable_get("@#{key.to_s}".to_sym).to_s == value.to_s
+      end
+    end
+    !temp_ary.include?(false)
   end
 
 end
